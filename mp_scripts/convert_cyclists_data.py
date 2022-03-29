2c969291ced21423a790f2284bd474557202d434
matthias p <matthias.pfuff@7p-group.com>
2022-03-29 15:26:02 +0200

    convert to method

--- mp_scripts/convert_cyclists_data.py
+++ mp_scripts/convert_cyclists_data.py
@@ -6,48 +6,55 @@ in_path = "/Users/matthias/projects/ml/vision/detection/tracking/data/cyclist/an
 out_path = "/Users/matthias/projects/ml/vision/detection/tracking/data/cyclist_mod/annotations/"
 out_path = in_path
 
-# in_file = "cyc_val.json"
-# out_file = "val.json"
-# seq_length = 886
 
-in_file = "cyc_train.json"
-out_file = "train.json"
-seq_length = 885
+def convert_files(in_path, out_path, in_file: str, out_file: str, seq_length: int):
+    in_file_path = in_path + in_file
 
-in_file_path = in_path + in_file
-out_file_path = out_path + in_file
+    with open(in_file_path, 'r') as f:
+        data = json.load(f)
+        # data['id_man'] = 134 # <--- add `id` value.
+        annotations = data['annotations']
+        for ann in annotations:
+            ann["image_id"] = ann["image_id"] - images_base_num
+            bbox = ann["bbox"]
+            for i, x in enumerate(bbox):
+                bbox[i] = int(bbox[i])
 
+        images = data['images']
+        for img in images:
+            img["seq_length"] = seq_length
+            img["first_frame_image_id"] = 0
+            img["id"] = img["id"] - images_base_num
+            img["prev_image_id"] = img["prev_image_id"] - images_base_num
+            img["next_image_id"] = img["next_image_id"] - images_base_num
+            img["frame_id"] = img["frame_id"] - images_base_num
 
-with open(in_file_path, 'r') as f:
-    data = json.load(f)
-    # data['id_man'] = 134 # <--- add `id` value.
-    annotations = data['annotations']
-    for ann in annotations:
-        ann["image_id"] = ann["image_id"] - images_base_num
-        bbox = ann["bbox"]
-        for i, x in enumerate(bbox):
-            bbox[i] = int(bbox[i])
+        # f.seek(0)        # <--- should reset file position to the beginning.
+        # json.dump(data, f, indent=4)
+        # f.truncate()     # remove remaining part
 
-    images = data['images']
-    for img in images:
-        img["seq_length"] = seq_length
-        img["first_frame_image_id"] = 0
-        img["id"] = img["id"] - images_base_num
-        img["prev_image_id"] = img["prev_image_id"] - images_base_num
-        img["next_image_id"] = img["next_image_id"] - images_base_num
-        img["frame_id"] = img["frame_id"] - images_base_num
-
-    # f.seek(0)        # <--- should reset file position to the beginning.
-    # json.dump(data, f, indent=4)
-    # f.truncate()     # remove remaining part
-
-# write to new file
-out_file = os.path.join(os.path.dirname(out_path), out_file)
-with open(out_file, 'w') as f:
-    json.dump(data, f, indent=4)
+    # write to new file
+    out_file = os.path.join(os.path.dirname(out_path), out_file)
+    with open(out_file, 'w') as f:
+        json.dump(data, f, indent=4)
 
 
+def convert_all(in_path, out_path):
+    in_train = {
+        "in_file": "cyc_train.json",
+        "out_file": "train.json",
+        "seq_length": 885
+    }
+    in_val = {
+        "in_file": "cyc_val.json",
+        "out_file": "val.json",
+        "seq_length": 886
+    }
+
+    files_list = [in_train, in_val]
+
+    for f in files_list:
+        convert_files(in_path, out_path, **f)
 
 
-
-
+convert_all(in_path, out_path)
--- mp_scripts/convert_cyclists_data_remote.py
+++ mp_scripts/convert_cyclists_data_remote.py
@@ -1,49 +1,12 @@
 import os, json
 
+from mp_scripts.convert_cyclists_data import convert_all
+
 images_base_num = 887
 
 in_path = "/home/ubuntu/projects/pytorch/tracking/data/cyclist/annotations/"
 out_path = "/home/ubuntu/projects/pytorch/tracking/data/cyclist_mod/annotations/"
 out_path = in_path
 
-in_file = "cyc_val.json"
-out_file = "val.json"
-seq_length = 886
 
-# in_file = "cyc_train.json"
-# out_file = "train.json"
-# seq_length = 885
-
-in_file_path = in_path + in_file
-out_file_path = out_path + in_file
-
-
-with open(in_file_path, 'r') as f:
-    data = json.load(f)
-    # data['id_man'] = 134 # <--- add `id` value.
-    annotations = data['annotations']
-    for ann in annotations:
-        ann["image_id"] = ann["image_id"] - images_base_num
-        bbox = ann["bbox"]
-        for i, x in enumerate(bbox):
-            bbox[i] = int(bbox[i])
-
-    images = data['images']
-    for img in images:
-        img["seq_length"] = seq_length
-        img["first_frame_image_id"] = 0
-        img["id"] = img["id"] - images_base_num
-        img["prev_image_id"] = img["prev_image_id"] - images_base_num
-        img["next_image_id"] = img["next_image_id"] - images_base_num
-        img["frame_id"] = img["frame_id"] - images_base_num
-
-# write to new file
-out_file = os.path.join(os.path.dirname(out_path), out_file)
-with open(out_file, 'w') as f:
-    json.dump(data, f, indent=4)
-
-
-
-
-
-
+convert_all(in_path, out_path)
