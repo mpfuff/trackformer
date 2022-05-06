be8f212941a86518a01734501a47993557c6aa3d
matthias p <matthias.pfuff@7p-group.com>
2022-05-06 20:00:25 +0200

    visdom pwd 2

--- mp_scripts/start_visdom.sh
+++ mp_scripts/start_visdom.sh
@@ -1,4 +1,2 @@
-export VISDOM_USERNAME=mobe
-export VISDOM_PASSWORD=interpc
-export VISDOM_COOKIE=~/.visdom/cookie
-VISDOM_USE_ENV_CREDENTIALS=1 visdom -enable_login -force_new_cookie -port 8090
+
+visdom -port 8090
